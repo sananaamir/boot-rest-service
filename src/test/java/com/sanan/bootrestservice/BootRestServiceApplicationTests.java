0c19eca925da899b0d4c3aefdae5d0c037d33708
Sanan Aamir <sananaamir@gmail.com>
2020-07-19 21:11:33 -0500

    initial commit

---
+++ src/test/java/com/sanan/bootrestservice/BootRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanan.bootrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
